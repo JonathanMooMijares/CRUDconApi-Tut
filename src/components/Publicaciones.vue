ee98ad4a4386f7c64063c787a129de5d8b984f67
JonathanMooMijares <l22390450@chetumal.tecnm.mx>
2026-05-26 02:11:38 -0500

    paso 2 completado, creación y definición de rutas

---
+++ src/components/Publicaciones.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Todas las Publicaciones</h1>
+    <table id="posts">
+        <tr>
+            <th>S.N</th>
+            <th>Titulo</th>
+            <th>Acción</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Hola Mundo!</td>
+            <td>
+                <button class="btn btn-green">0</button>
+                <button class="btn btn-blue">/</button>
+                <button class="btn btn-red">x</button>
+            </td>
+        </tr>
+    </table>
+</template>
