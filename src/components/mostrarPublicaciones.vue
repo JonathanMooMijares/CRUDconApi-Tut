2e452bf709c2a2aae9a5d5f796d256dbb9161058
JoEdMM <l22390450@chetumal.tecnm.mx>
2026-05-26 12:25:20 -0500

    agregación componeten del select

---
+++ src/components/mostrarPublicaciones.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+import { onMounted, reactive } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const publicacionId = route.params.id;
+const state = reactive({
+    publicacion: Object
+})
+
+onMounted(async () => {
+    try{
+        const response = await axios.get(`http://localhost:8080/publicaciones/${publicacionId}`);
+        state.post = responde.data;
+    }catch(error){
+        console.log('Error en el Fetch', error);
+    }
+});
+</script>
+
+<template>
+    <div class="center-container">
+        <h1>{{ state.publicacion.title }}</h1>
+        <p>{{ state.publicacion.description }}</p>
+        <RouterLink :to="`/publicaciones`" class="btn btn-green">Regresar</RouterLink>
+    </div>
+</template>
