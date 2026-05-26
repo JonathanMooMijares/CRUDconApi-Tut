c41c232c6418fdaf6624229b79a3ee57de84ac67
JonathanMooMijares <l22390450@chetumal.tecnm.mx>
2026-05-26 02:01:29 -0500

    Creación de la introducción al RouterLink

---
+++ src/Home.vue
@@ -0,0 +1,10 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div class ="center-container">
+        <h1>Bienvenido a un tutorial de un CRUD en Vue.js</h1>
+        <RouterLink :to="`/posts`" class="btn btn-green">Ver los post</RouterLink>
+    </div>
+</template>
